8b429f7a2466018877f743637125374903d862de
wangdong <1728001662@qq.com>
2025-12-12 14:44:44 +0800

    add: leetcode hot100.56. 合并区间

--- src/main/java/com/wd/leetcode/hot100/Exercise.java
+++ src/main/java/com/wd/leetcode/hot100/Exercise.java
@@ -1,5 +1,9 @@
 package main.java.com.wd.leetcode.hot100;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise {
 
     public int maxSubArray(int[] nums) {
@@ -10,5 +14,26 @@ public class Exercise {
             res = Math.max(res, curr);
         }
         return res;
+    }
+
+    //    输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+    //    输出：[[1,6],[8,10],[15,18]]
+    //    解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        List<int[]> res = new ArrayList<>();
+        for (int[] interval : intervals) {
+            int size = res.size();
+            if (size > 0 && interval[0] <= res.get(size - 1)[1]) {
+                res.get(size - 1)[1] = Math.max(res.get(size - 1)[1], interval[1]);
+            } else {
+                res.add(interval);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+
+    public static void main(String[] args) {
+        merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}});
     }
 }
