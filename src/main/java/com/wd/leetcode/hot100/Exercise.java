896e14e0edc85f98cb9d77f02a3f71bb7652172d
wangdong <60547953+172800wd@users.noreply.github.com>
2025-12-30 15:10:33 +0800

    add: 73. 矩阵置零

--- src/main/java/com/wd/leetcode/hot100/Exercise.java
+++ src/main/java/com/wd/leetcode/hot100/Exercise.java
@@ -86,7 +86,56 @@ public class Exercise {
         return length + 1;
     }
 
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean firstRow = false;
+        boolean firstColumn = false;
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRow = true;
+                break;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColumn = true;
+                break;
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (firstRow) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (firstColumn) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(firstMissingPositive(new int[]{100000, 3, 4000, 2, 15, 1, 99999}));
+        setZeroes(new int[][]{
+                {0, 1, 2, 0},
+                {3, 4, 5, 6},
+                {7, 8, 9, 1}
+        });
+        System.out.println();
     }
 }
